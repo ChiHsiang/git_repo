34a384feb546597adc11312d8ab162c57d4a1314
ChiHsiang <1207victory@gmail.com>
2016-08-21 13:09:56 +0800

    test

--- create.sh
+++ create.sh
@@ -2,13 +2,8 @@
 
 while read date
 do
-    # fileName=`echo "$date" | tr " " "_"`
-    # date="$date 14:00 2013 +0500"
-    # echo "Creating file... $fileName"
-
     echo "$date"
     count=$(jot -r 1  1 10)
-    # touch "$fileName"
 
     for ((i=1; i<=$count; i=i+1))
     do
@@ -16,6 +11,4 @@ do
       git add . 
       git commit --date="$date" -m "$date"
     done
-    # git add "$fileName"
-    # git commit --date="$date" --author="public-contributions" -m "$fileName"
 done < dates.txt
